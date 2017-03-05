9a8c6ec5de39e39295aec22f8f02c79b21e36bf4
Austin L.Gable <zhilinchn@gmail.com>
2017-03-05 19:10:34 +0800

    Sunday commit

--- expandablecalendar/src/main/java/com/banksy/expandablecalendar/ExpandableAdapter.java
+++ expandablecalendar/src/main/java/com/banksy/expandablecalendar/ExpandableAdapter.java
@@ -1,17 +1,38 @@
 package com.banksy.expandablecalendar;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.lang.ref.WeakReference;
+import java.util.Calendar;
 
 /**
  * Created by orange on 3/5/17.
  */
 
 public class ExpandableAdapter extends BaseAdapter {
+  private static final String TAG = "ExpandableAdapter";
+  private WeakReference<Context> mContext ;
+  private SparseArray<String> mWeekList = new SparseArray<>();
+  private boolean isExpand = false;
+
+
+  private int selection ;
+  private final Calendar mCalendar;
+
+  public ExpandableAdapter(Context context) {
+    mContext = new WeakReference<Context>(context);
+    mCalendar = Calendar.getInstance();
+
+  }
 
   @Override public int getCount() {
-    return 0;
+    return mWeekList.size();
   }
 
   @Override public Object getItem(int position) {
@@ -23,6 +44,45 @@ public class ExpandableAdapter extends BaseAdapter {
   }
 
   @Override public View getView(int position, View convertView, ViewGroup parent) {
-    return null;
+
+    Log.d(TAG, "getView: parent == " + (parent == null));
+
+    ViewHolder viewHolder ;
+
+    if (convertView == null) {
+      convertView = View.inflate(mContext.get(), R.layout.adatpter__calender__, null);
+      viewHolder = new ViewHolder(convertView);
+      convertView.setTag(viewHolder);
+    }else {
+      viewHolder = (ViewHolder) convertView.getTag();
+    }
+
+    if (selection == position) {
+      viewHolder.tv.setSelected(true);
+      viewHolder.tv.setTextColor(Color.WHITE);
+      viewHolder.tv.setBackgroundResource(R.drawable.calendar__shape_circle_red);
+    }else {
+      viewHolder.tv.setSelected(false);
+      viewHolder.tv.setTextColor(Color.BLACK);
+      viewHolder.tv.setBackgroundColor(Color.TRANSPARENT);
+    }
+
+
+    return convertView;
+  }
+  public int getSelection() {
+    return selection;
+  }
+
+  public void setSelection(int selection) {
+    this.selection = selection;
+  }
+
+
+  private static class ViewHolder {
+    private TextView tv;
+    public ViewHolder(View view) {
+      tv = (TextView) view.findViewById(R.id.tv);
+    }
   }
 }
--- expandablecalendar/src/main/java/com/banksy/expandablecalendar/MainActivity.java
+++ expandablecalendar/src/main/java/com/banksy/expandablecalendar/MainActivity.java
@@ -1,12 +1,75 @@
 package com.banksy.expandablecalendar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String TAG = "MainActivity";
+  private ViewFlipper mViewFlipper;
+  private GestureDetectorCompat gestureDetector;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mViewFlipper = (ViewFlipper) findViewById(R.id.mViewFlipper);
+    gestureDetector = new GestureDetectorCompat(this, onGestureListener);
+    mViewFlipper.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+      }
+    });
+  }
+
+
+  GestureDetector.OnGestureListener onGestureListener = new MyOnGestureListener(){
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+      Log.d(TAG, "onFling: Fling in >>>>>>");
+      if (e1.getX() - e2.getX() > 80) {
+        Log.d(TAG, "onFling: >>>>>> fling ring");
+        return true;
+      }else if (e1.getX() - e2.getX() < -80){
+        Log.d(TAG, "onFling: >>>>>> fling Left");
+        return true;
+      }
+      return false;
+    }
+  };
+
+
+  static class MyOnGestureListener implements  GestureDetector.OnGestureListener{
+
+    @Override public boolean onDown(MotionEvent e) {
+      return false;
+    }
+
+    @Override public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override public boolean onSingleTapUp(MotionEvent e) {
+      return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+      return false;
+    }
+
+    @Override public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+      return false;
+    }
   }
 }
